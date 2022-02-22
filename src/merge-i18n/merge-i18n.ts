9cf3da1e103e922039fafd83d4fc0ad50196f2ae
cwaespi <claudio.waespi@bluewin.ch>
2022-02-22 12:36:19 +0000

    feat(merge-i18n): rebuild xlf-merge

--- src/merge-i18n/merge-i18n.ts
+++ src/merge-i18n/merge-i18n.ts
@@ -1,17 +1,15 @@
 import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/architect';
 import { JsonObject } from '@angular-devkit/core';
-import { rmSync } from 'fs';
-import { concat } from './xlf-concat';
-import { merge } from './xlf-merge';
+import { executeXLF } from './xlf-merge';
 import { log } from './utils';
 
 interface Options extends JsonObject {
   preventExtractI18n: boolean;
 }
 
-export default createBuilder(copyFileBuilder);
+export default createBuilder(mergeI18nBuilder);
 
-async function copyFileBuilder(
+async function mergeI18nBuilder(
   options: Options,
   context: BuilderContext,
 ): Promise<BuilderOutput> {
@@ -36,27 +34,24 @@ async function copyFileBuilder(
     log(`...extracted i18n successfully`);
   }
 
-  log(`merge i18n...`);
-
-  const projectMetadata = await context.getProjectMetadata(project);
-  const i18n = projectMetadata.i18n as JsonObject;
-  const sourceLocale = i18n?.sourceLocale as string;
-  const locales = i18n?.locales as JsonObject;
-
-  const tmpCompactSourceLocalFile = `${outFile}.tmp`;
-
-  if (locales) {
-    concat(outFile, tmpCompactSourceLocalFile, sourceLocale);
-
-    Object.keys(locales)?.forEach(locale => {
-      const file = locales[locale] as string;
-      log(`merge i18n ${sourceLocale} (${outFile}) with ${locale} (${file})`);
-      merge(tmpCompactSourceLocalFile, file, locale);
-    });
-
-    rmSync(tmpCompactSourceLocalFile);
+  if (format === 'xlf') {
+    log(`merge i18n...`);
+    const projectMetadata = await context.getProjectMetadata(project);
+    const i18n = projectMetadata.i18n as JsonObject;
+    const sourceLocale = i18n?.sourceLocale as string;
+    const locales = i18n?.locales as JsonObject;
+  
+    if (locales) {
+      Object.keys(locales)?.forEach(locale => {
+        const file = locales[locale] as string;
+        log(`merge i18n ${sourceLocale} (${outFile}) with ${locale} (${file})`);
+        executeXLF(outFile, file, locale);
+      });
+    }
+  
+    log('...merged i18n successfully.');
+    return { success: true };
   }
 
-  log('...merged i18n successfully.');
-  return { success: true };
+  return { success: false, error: `format ${format} no support` };
 }
